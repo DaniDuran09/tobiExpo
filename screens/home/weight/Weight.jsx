010f6df479b98fb546c0620104474ebabd9c37f8
Johan Antunez <johan.gonzalez@boletia.com>
2024-12-05 09:50:12 -0600

    update: refactor weight/welfare screens

--- screens/home/weight/Weight.jsx
+++ screens/home/weight/Weight.jsx
@@ -1,4 +1,3 @@
-import { Image, StyleSheet, ScrollView} from "react-native";
 import React, { useCallback, useEffect, useState } from "react";
 import { Colors } from "../../../styles/Colors";
 import Recomendation from "../../../components/Recomendation";
@@ -8,7 +7,8 @@ import { calculateIdealWeight } from "../../../utils/scripts";
 import CorrectWeight from "../../../components/pet/CorrectWeight";
 import ApiFetcher from "../../../modules/ApiFetcher";
 import Loading from "../../../components/Loading";
-import { View,Card,Text,TouchableOpacity } from "react-native-ui-lib";
+import { View, Text, TouchableOpacity, } from "react-native-ui-lib";
+import { CardInfo } from "../../../components/pet/CardInfo";
 
 const Weight = (props) => {
   const { item } = props;
@@ -27,7 +27,11 @@ const Weight = (props) => {
   const rangeOne = pet?.ideal_weight?.from / 1000;
   const rangeTwo = pet?.ideal_weight?.to / 1000;
 
-  const realWeight = calculateIdealWeight(rangeOne, rangeTwo, parseInt(pet?.weight));
+  const realWeight = calculateIdealWeight(
+    rangeOne,
+    rangeTwo,
+    parseInt(pet?.weight)
+  );
 
   const getPet = async () => {
     setLoading(true);
@@ -48,115 +52,73 @@ const Weight = (props) => {
     }, [navigation])
   );
 
-
   useEffect(() => {
     setSuccess(realWeight?.ideal);
   }, [pet]);
 
   return (
-    <ScrollView style={styles.container} contentContainerStyle={styles.contentContainer}>
-     
+    <View flex backgroundColor={Colors.white} padding-15>
       {loading && (
         <Loading
           textColor={Colors.primaryColor}
           backgroundColorProp={Colors.white}
         />
       )}
-        <CardInfo title="Real" image={require("../../../assets/balance.png")}>
-          <Text
-            marginT-20
-            text60BO
-            color={realWeight?.ideal?Colors.green:Colors.primaryColor}                          
-            >
-              {parseInt(pet.weight)} kg
-            </Text>           
-            <Text marginT-25 style={styles.lastUpdate}>Último registro: {pet.weight_updated_at ? pet.weight_updated_at : "Sin fecha"}</Text>            
+      <CardInfo title="Real" image={require("../../../assets/balance.png")}>
+        <Text
+          text60BO
+          marginT-10
+          color={realWeight?.ideal ? Colors.green : Colors.primaryColor}
+        >
+          {parseInt(pet.weight)} kg
+        </Text>
+        <Text marginT-15>
+          Último registro:{" "}
+          {pet.weight_updated_at ? pet.weight_updated_at : "Sin fecha"}
+        </Text>
 
-            <TouchableOpacity
-              marginT-10              
-              onPress={() =>
-                navigation.navigate("EditPet", {
-                  id: item.id,
-                })
-              }
-            >
-              <Text underline style={styles.updateTetx}>Actualizar peso real</Text>            
-            </TouchableOpacity>
-        </CardInfo>
-
-        <CardInfo title="Rango ideal" image={require("../../../assets/balance.png")}>
-          <Text
-              marginT-10
-              text80BO              
-            >{`${rangeOne} Kg - ${rangeTwo} Kg`}
-          </Text>
-        </CardInfo>
-              
-        {success ? (
-          <CorrectWeight />
-        ) : (
-          <Recomendation
-            title={"Recomendación"}
-            info={
-              "Programa una cita con un especialista en nutrición para el cuidado de tu mascota."
-            }
-            setVisible={setChallengeVisible}
-            oneOption={true}
-          />
-        )}
-        <ChallengeModal
-          closeModalChallenge={closeModalChallenge}
-          challengeVisible={challengeVisible}
-          text={
-            "El bienestar de tu mascota es lo más importante. Consulta a un especialista para asegurarte de que esté en el rango ideal."
+        <TouchableOpacity
+          marginT-10
+          onPress={() =>
+            navigation.navigate("EditPet", {
+              id: item.id,
+            })
           }
-        />      
-    </ScrollView>
+        >
+          <Text text80M underline>
+            Actualizar peso real
+          </Text>
+        </TouchableOpacity>
+      </CardInfo>
+
+      <CardInfo
+        title="Rango ideal"
+        image={require("../../../assets/balance.png")}
+      >
+        <Text text80BO marginT-10>{`${rangeOne} Kg - ${rangeTwo} Kg`}</Text>
+      </CardInfo>
+
+      {success ? (
+        <CorrectWeight />
+      ) : (
+        <Recomendation
+          title={"Recomendación"}
+          info={
+            "Programa una cita con un especialista en nutrición para el cuidado de tu mascota."
+          }
+          setVisible={setChallengeVisible}
+          oneOption={true}
+        />
+      )}
+      <ChallengeModal
+        closeModalChallenge={closeModalChallenge}
+        challengeVisible={challengeVisible}
+        text={
+          "El bienestar de tu mascota es lo más importante. Consulta a un especialista para asegurarte de que esté en el rango ideal."
+        }
+      />
+    </View>
   );
 };
 
-const CardInfo = ({title,image,children})=>{
-  return (
-    <Card padding-15 br8 backgroundColor={Colors.mediumWhite} style={styles.realWeightContainer} >
-      <View row centerV>
-          <Image
-            source={image}
-            style={styles.imageBalance}                       
-          />
-          <Text text65 style={styles.realText}>{title}</Text>
-      </View>
-      {children}
-    </Card>
-  )
-}
-
 export default Weight;
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,    
-  },
-  contentContainer:{
-    backgroundColor: Colors.white,
-    padding:15
-  },
-  realWeightContainer: {   
-    marginBottom: "5%",
-  },
-  imageBalance: {
-    width:20,
-    height:20,
-  }, 
-  realText: {
-    fontSize: 18,
-    marginLeft: 10,
-    fontWeight: "600",
-  },
- 
-  lastUpdate: {    
-    fontWeight: "300",
-  },
-  updateTetx: {
-    fontWeight: "500",
-  },
-});
--- screens/home/welfare/Welfare.jsx
+++ screens/home/welfare/Welfare.jsx
@@ -1,17 +1,13 @@
-import {StyleSheet } from "react-native";
 import React, { useState } from "react";
 import ChallengeModal from "../../../components/ChallengeModal";
 import Recomendation from "../../../components/Recomendation";
-import { View,Text } from "react-native-ui-lib";
+import { View, Text } from "react-native-ui-lib";
 
 const Welfare = () => {
   const [challengeVisible, setChallengeVisible] = useState(false);
-  const closeModalChallenge = () => {
-    console.log("Me presiono")
-    setChallengeVisible(false);
-  };
+  const closeModalChallenge = () => setChallengeVisible(false);
   return (
-    <View flex padding-10 style={styles.container}>
+    <View flex padding-10 marginT-10>
       <Text text65BO marginB-15>
         ¿Con qué frecuencia debo bañar y asear a mi mascota?
       </Text>
@@ -20,25 +16,19 @@ const Welfare = () => {
         una vez por mes.
       </Text>
       <Recomendation
-            title={"GROOMING, SPA Y ESTÉTICA"}
-            info={
-              "¿Quieres programar un baño/aseo para tu mascota?"
-            }
-            setVisible={setChallengeVisible}
-          />
+        title={"GROOMING, SPA Y ESTÉTICA"}
+        info={"¿Quieres programar un baño/aseo para tu mascota?"}
+        setVisible={setChallengeVisible}
+      />
       <ChallengeModal
         closeModalChallenge={closeModalChallenge}
         challengeVisible={challengeVisible}
-        text={"En los días sin baño mantén el deslanado para evitar la formación de nudos."}
+        text={
+          "En los días sin baño mantén el deslanado para evitar la formación de nudos."
+        }
       />
     </View>
   );
 };
 
 export default Welfare;
-
-const styles = StyleSheet.create({
-  container: {   
-    marginTop: "8%",
-  }, 
-});
