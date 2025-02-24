26d5a1c200446daa0b259173eb70fdb9501fdbb3
AaronGT03 <aarongalarza2000@gmail.com>
2025-02-23 23:29:37 -0600

    commit

--- components/partners/ServicesData.ts
+++ components/partners/ServicesData.ts
@@ -1,18 +1,17 @@
 const ServicesData = [
     {
-      imageSource: require("../../assets/vetBackground.png"),
-      title: "Veterinarios",
-      subtitle: "Certificados",
-      type: 2,
+        imageSource: require("../../assets/vetBackground.png"),
+        title: "Veterinarios",
+        subtitle: "Certificados",
+        type: 2,
     },
     {
-      imageSource: require("../../assets/groomingBackground.png"),
-      title: "Grooming",
-      subtitle: "Spa, baños y estética",
-      type: 3,
+        imageSource: require("../../assets/groomingBackground.png"),
+        title: "Grooming",
+        subtitle: "Spa, baños y estética",
+        type: 3,
     },
-    
-  ];
-  
-  export default ServicesData;
-  
+
+];
+
+export default ServicesData;
