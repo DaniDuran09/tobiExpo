1441a4f228ce18245821ed7e205be095e747becd
AaronGT03 <aarongalarza2000@gmail.com>
2025-02-28 22:03:35 -0600

    Update DetailsScreen.tsx

--- screens/DetailsScreen/DetailsScreen.tsx
+++ screens/DetailsScreen/DetailsScreen.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import { FlatList, RefreshControl } from "react-native";
 import { View } from "react-native-ui-lib"; 
 import { Colors } from "../../styles/Colors";
