dbcb1b10574993743487d173b9794291c7e864c0
AaronGT03 <aarongalarza2000@gmail.com>
2025-03-01 00:17:03 -0600

    Update BottomMenu

--- components/user/BottonMenu/BottomMenu.tsx
+++ components/user/BottonMenu/BottomMenu.tsx
@@ -1,6 +1,6 @@
-import { TouchableOpacity, Image, View, FlatList, StyleSheet } from "react-native";
+import { Image, FlatList } from "react-native";
 import React from "react";
-import { Text } from "react-native-ui-lib";
+import { Button, Text, View } from "react-native-ui-lib";
 import { useNavigation } from "@react-navigation/native";
 import { StackNavigationProp } from "@react-navigation/stack";
 import activitiesData from "../../../utils/data/activitiesData"; // Importa las actividades
@@ -25,59 +25,27 @@ const BottomMenu = ({ disabledOption = false }: BottomMenuProps) => {
   };
 
   return (
-    <View style={styles.container}>
-      <TouchableOpacity onPress={addNewPet}>
-        <Text marginT-5>+ Mascotas</Text>
-      </TouchableOpacity>
-      <View style={styles.header}>
-        <Text text70BO black marginT-5>
+    <View flex bg-white paddingH-10 left>
+      <Button
+        label="+ Mascotas"
+        onPress={addNewPet}
+        link
+        black
+      />
+      <View row width="100%"  marginT-2>
+        <Text text70BO black>
           MI ACTIVIDAD
         </Text>
       </View>
 
       <FlatList
-        data={activitiesData}  // Usamos activitiesData importado
+        data={activitiesData}
         renderItem={({ item }) => renderActivityItem({ item, disabledOption, screenNavigate })}
         keyExtractor={(item, index) => index.toString()}
-        ItemSeparatorComponent={() => <View style={styles.separator} />}
+       
       />
     </View>
   );
 };
 
 export default BottomMenu;
-
-const styles = StyleSheet.create({
-  container: {
-    backgroundColor: "#fff",
-    height: "33%",
-    width: "100%",
-    justifyContent: "flex-start",
-    alignItems: "flex-start",
-    paddingHorizontal: 10,
-  },
-  header: {
-    flexDirection: "row",
-    width: "100%",
-    alignItems: "center",
-  },
-  section: {
-    height: 60,
-    width: "100%",
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "center", // Aquí debe ser "center" y no "string"
-    paddingVertical: 10,
-  },
-  image: { height: 20, width: 20, marginRight: 10 },
-  text: {
-    flex: 1,
-    fontWeight: "bold",
-  },
-  arrowImage: { height: 15, width: 15 },
-  separator: {
-    height: 1,
-    backgroundColor: "grey",
-    marginVertical: 10,
-  },
-});
