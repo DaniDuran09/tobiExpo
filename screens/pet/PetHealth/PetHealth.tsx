f24d7131d0cc710c5b6565c228b5bed3a011d212
AaronGT03 <aarongalarza2000@gmail.com>
2025-04-13 15:54:37 -0600

    fix: Carga inicial y post-actualización de datos de mascota
    -Creacion de funcion UPDATE para datos de mascota

--- screens/pet/PetHealth/PetHealth.tsx
+++ screens/pet/PetHealth/PetHealth.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState, useEffect } from "react";
 import { ScrollView, TextInput, TouchableOpacity } from "react-native";
 import { View, Text, Button } from "react-native-ui-lib";
 import { Avatar, RadioButton } from "react-native-paper";
@@ -6,9 +6,10 @@ import { MaterialCommunityIcons } from "@expo/vector-icons";
 import { useRoute } from "@react-navigation/native";
 import Icon from "react-native-vector-icons/MaterialCommunityIcons";
 import { useNavigation } from "@react-navigation/native";
-import { useGetPetsQuery } from "../../../services/api/pets.api";
-import { Colors} from "../../../styles/Colors";
+import { Colors } from "../../../styles/Colors";
 import Loading from "../../../components/Loading";
+import Toast from "react-native-toast-message";
+import ApiFetcher from "../../../modules/ApiFetcher";
 
 const PetProfile = () => {
   const navigation = useNavigation();
@@ -16,34 +17,85 @@ const PetProfile = () => {
   const { item } = route.params as { item: Pet };
 
   const [selectedPet, setSelectedPet] = useState<Pet>(item);
-  const [isSterilized, setIsSterilized] = useState<boolean>(
-    item?.sterilized ?? false
-  );
+  const [petInfo, setPetInfo] = useState({
+    name: item?.name || "",
+    weight: item?.weight || 0,
+    sterilized: item?.sterilized ?? false,
+  });
+  const [loadData, setLoadData] = useState(false);
+  const apiFetcher = new ApiFetcher();
 
-  const { data: petsResponse, isLoading: isLoadingPets } = useGetPetsQuery();
-  const pets = petsResponse?.data || [];
-  useEffect(() => {
-    if (item?.id && pets.length > 0) {
-      const updatedPet = pets.find((pet: Pet) => pet.id === item.id);
-      if (updatedPet) {
-        setSelectedPet(updatedPet);
-        setIsSterilized(updatedPet?.sterilized ?? false);
+  const getPetInfo = async () => {
+    setLoadData(true);
+    try {
+      const response = await apiFetcher.getPetById(selectedPet.id);
+      if (response.code == 200) {
+        setPetInfo(response.data);
+      } else {
+        console.log("Algo salió mal");
       }
+    } catch (error) {
+      console.log("Error: ", error);
+      Toast.show({
+        type: "error",
+        text1: "No se pudo cargar la información",
+        text2: `Inténtalo de nuevo más tarde`,
+      });
+      navigation.goBack();
+    } finally {
+      setLoadData(false);
     }
-  }, [pets]);
+  };
 
-  if (isLoadingPets) {
+  useEffect(() => {
+    getPetInfo();
+  }, []);
+  const rawGender = selectedPet?.pet_breed?.life_stages?.[0]?.gender;
+  const petGender =
+    rawGender === "male"
+      ? "Macho"
+      : rawGender === "female"
+      ? "Hembra"
+      : "Sin especificar";
+  const updatePet = async () => {
+    setLoadData(true);
+    try {
+      const response = await apiFetcher.updatePet(selectedPet.id, petInfo);
+
+      if (response.code === 200) {
+        Toast.show({
+          type: "success",
+          text1: "Guardado",
+          text2: `Se actualizó la información de ${response.data.name}`,
+        });
+        getPetInfo();
+        navigation.goBack();
+      }
+    } catch (error) {
+      console.log("Error al actualizar mascota:", error);
+      Toast.show({
+        type: "error",
+        text1: "Error",
+        text2: "Mascota no actualizada",
+      });
+    } finally {
+      setLoadData(false);
+    }
+  };
+
+  if (loadData) {
     return (
-      <Loading
-        backgroundColor={Colors.white}
-        textColor={Colors.primaryColor}
-      />
+      <Loading backgroundColor={Colors.white} textColor={Colors.primaryColor} />
     );
   }
 
   return (
     <ScrollView style={{ flex: 1, backgroundColor: "#fff" }}>
-      <View row padding-20>
+      <View
+        row
+        padding-20
+        style={{ borderBottomWidth: 1, borderBottomColor: Colors.secondGray }}
+      >
         <View center>
           <Avatar.Image source={{ uri: selectedPet?.picture }} size={100} />
           <Text text80 color={Colors.gray} marginT-10>
@@ -52,10 +104,11 @@ const PetProfile = () => {
         </View>
 
         <View marginL-10 centerV>
-          <Text text40BL>{selectedPet?.name}</Text>
-
+          <Text text40BL>{petInfo.name}</Text>
           <Text text80L color={Colors.gray} marginT-5>
-            {`${selectedPet?.age} años | ${selectedPet?.gender} | ${selectedPet?.pet_breed}`}
+            {`${selectedPet?.age} años | ${petGender} | ${
+              petInfo.sterilized ? "Esterilizado" : "No esterilizado"
+            }`}
           </Text>
         </View>
       </View>
@@ -68,44 +121,10 @@ const PetProfile = () => {
           placeholder="Nombre"
           placeholderTextColor="#000"
           elevation={5}
-          value={selectedPet?.name}
-          style={[
-            {
-              color: "#000",
-              height: 60,
-              width: "90%",
-              paddingLeft: 20,
-              justifyContent: "center",
-              backgroundColor: "#D6EFFF",
-              borderRadius: 4,
-            },
-          ]}
-          autoCapitalize="none"
-        />
-        <Text marginB-5 marginT-20 text80>
-          Peso de tu mascota
-        </Text>
-        <TextInput
-          placeholder="Peso"
-          placeholderTextColor="#000"
-          elevation={5}
-          value={selectedPet?.weight}
-          style={[
-            {
-              color: "#000",
-              height: 60,
-              width: "90%",
-              paddingLeft: 20,
-              justifyContent: "center",
-              backgroundColor: "#D6EFFF",
-              borderRadius: 4,
-            },
-          ]}
-          autoCapitalize="none"
-        />
-        <View
-          marginT-20
+          value={petInfo.name}
+          onChangeText={(text) => setPetInfo({ ...petInfo, name: text })}
           style={{
+            color: "#000",
             height: 60,
             width: "90%",
             paddingLeft: 20,
@@ -113,13 +132,49 @@ const PetProfile = () => {
             backgroundColor: "#D6EFFF",
             borderRadius: 4,
           }}
+          autoCapitalize="none"
+        />
+
+        <Text marginB-5 marginT-20 text80>
+          Peso de tu mascota
+        </Text>
+        <TextInput
+          placeholder="Peso"
+          placeholderTextColor="#000"
+          elevation={5}
+          value={petInfo.weight}
+          onChangeText={(text) => setPetInfo({ ...petInfo, weight: text })}
+          style={{
+            color: "#000",
+            height: 60,
+            width: "90%",
+            paddingLeft: 20,
+            justifyContent: "center",
+            backgroundColor: "#D6EFFF",
+            borderRadius: 4,
+          }}
+          autoCapitalize="none"
+        />
+
+        <View
+          marginT-20
+          height={60}
+          width={'90%'}
+          paddingL-20
+          backgroundColor="#D6EFFF"
+          style={{
+            justifyContent: "center",
+            borderRadius: 4,
+          }}
         >
           <View row centerV>
             <RadioButton.Android
               value="yes"
-              status={isSterilized ? "checked" : "unchecked"}
-              color="red"
-              onPress={() => setIsSterilized(!isSterilized)}
+              status={petInfo.sterilized ? "checked" : "unchecked"}
+              color={Colors.primaryColor}
+              onPress={() =>
+                setPetInfo({ ...petInfo, sterilized: !petInfo.sterilized })
+              }
             />
             <Text marginL-10>¿Tu mascota está esterilizada?</Text>
           </View>
@@ -129,15 +184,15 @@ const PetProfile = () => {
           row
           marginT-20
           width={"90%"}
-          style={{ justifyContent: "space-between", alignItems: "center" }}
+          style={{ justifyContent: "space-between"}}
         >
           <View row center>
             <MaterialCommunityIcons
               name="trash-can-outline"
               size={24}
-              color="lightgray"
+              color={Colors.gray}
             />
-            <Text grey40 marginL-5>
+            <Text color={Colors.gray} marginL-5>
               Eliminar mascota
             </Text>
           </View>
@@ -146,13 +201,14 @@ const PetProfile = () => {
             backgroundColor={Colors.primaryColor}
             label="Actualizar"
             style={{ borderRadius: 10, padding: 15 }}
+            onPress={updatePet}
           />
         </View>
       </View>
 
       <View paddingH-10>
         <View marginB-10>
-          <Text text70BO black marginT-5>
+          <Text text70BO marginT-5>
             MIS DOCUMENTOS
           </Text>
         </View>
