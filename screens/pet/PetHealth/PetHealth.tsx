747a040f89fbce0bb9c14ec256e2231f0ca67cc2
AaronGT03 <aarongalarza2000@gmail.com>
2025-04-21 20:51:08 -0600

    fix: PetHealth
    Implementacion de funcion "Editar Foto"

--- screens/pet/PetHealth/PetHealth.tsx
+++ screens/pet/PetHealth/PetHealth.tsx
@@ -1,7 +1,12 @@
 import React, { useState, useEffect } from "react";
-import { ScrollView, TextInput, TouchableOpacity } from "react-native";
-import { View, Text, Button } from "react-native-ui-lib";
-import { Avatar, RadioButton } from "react-native-paper";
+import {
+  ScrollView,
+  TextInput,
+  TouchableOpacity,
+  SafeAreaView,
+} from "react-native";
+import { View, Text, Button, Image } from "react-native-ui-lib";
+import { RadioButton } from "react-native-paper";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
 import { useRoute } from "@react-navigation/native";
 import Icon from "react-native-vector-icons/MaterialCommunityIcons";
@@ -10,12 +15,14 @@ import { Colors } from "../../../styles/Colors";
 import Loading from "../../../components/Loading";
 import Toast from "react-native-toast-message";
 import ApiFetcher from "../../../modules/ApiFetcher";
+import WithoutPhoto from "../../../components/WithoutPhoto";
+import ImageOption from "../../../components/ImageOption";
+import * as ImagePicker from "expo-image-picker";
 
 const PetProfile = () => {
   const navigation = useNavigation();
   const route = useRoute();
   const { item } = route.params as { item: Pet };
-
   const [selectedPet, setSelectedPet] = useState<Pet>(item);
   const [petInfo, setPetInfo] = useState({
     name: item?.name || "",
@@ -23,7 +30,126 @@ const PetProfile = () => {
     sterilized: item?.sterilized ?? false,
   });
   const [loadData, setLoadData] = useState(false);
+  const [modalVisible, setModalVisible] = useState(false);
+  const [imageSource, setImageSource] = useState(null);
+  const [permissionsRequested, setPermissionsRequested] = useState({
+    camera: false,
+    library: false,
+  });
   const apiFetcher = new ApiFetcher();
+
+  const getPermissionsLibrary = async () => {
+    const { status } = await ImagePicker.getMediaLibraryPermissionsAsync();
+    if (status !== "granted") {
+      if (!permissionsRequested.library) {
+        setPermissionsRequested((prev) => ({ ...prev, library: true }));
+        await requestLibraryPermissions();
+      }
+      closeModal();
+      Toast.show({
+        type: "error",
+        text2: `Se necesitan permisos para acceder a la biblioteca de imágenes.`,
+        text1: `Habilita los permisos desde la configuración.`,
+      });
+    } else {
+      selectImageFromLibrary();
+    }
+  };
+
+  const requestLibraryPermissions = async () => {
+    const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();
+    if (status !== "granted") {
+      closeModal();
+      Toast.show({
+        type: "error",
+        text2: `Se necesitan permisos para acceder a la biblioteca de imágenes.`,
+        text1: `Habilita los permisos desde la configuración.`,
+      });
+    } else {
+      getPermissionsLibrary();
+    }
+  };
+
+  const selectImageFromLibrary = async () => {
+    try {
+      const result = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.Images,
+        allowsEditing: true,
+        aspect: [4, 3],
+        quality: 1,
+      });
+
+      if (!result.canceled) {
+        setImageSource(result.assets[0]);
+        closeModal();
+      }
+    } catch (error) {
+      Toast.show({
+        type: "error",
+        text2: `Error`,
+        text1: `Error al cargar la foto`,
+      });
+      console.log("Error: ", error);
+    }
+  };
+  const getPermissionsCamera = async () => {
+    const { status } = await ImagePicker.getCameraPermissionsAsync();
+    if (status !== "granted") {
+      if (!permissionsRequested.camera) {
+        setPermissionsRequested((prev) => ({ ...prev, camera: true }));
+        await requestPermissionsCamera();
+      }
+      Toast.show({
+        type: "error",
+        text2: `Permisos insuficientes.`,
+        text1: `Se necesitan permisos para acceder a la cámara.`,
+      });
+    } else {
+      takePhoto();
+    }
+  };
+  const requestPermissionsCamera = async () => {
+    const { status } = await ImagePicker.requestCameraPermissionsAsync();
+    if (status !== "granted") {
+      closeModal();
+      Toast.show({
+        type: "error",
+        text2: `Permisos insuficientes.`,
+        text1: `Se necesitan permisos para acceder a la cámara.`,
+      });
+    } else {
+      getPermissionsCamera();
+    }
+  };
+
+  const takePhoto = async () => {
+    try {
+      const result = await ImagePicker.launchCameraAsync({
+        allowsEditing: true,
+        aspect: [4, 3],
+        quality: 1,
+      });
+
+      if (!result.canceled) {
+        console.log("result: ", result);
+        setImageSource(result.assets[0]);
+        closeModal();
+      }
+    } catch (error) {
+      Toast.show({
+        type: "error",
+        text2: `Error`,
+        text1: `Error al cargar la foto`,
+      });
+      console.log("Error: ", error);
+    }
+  };
+
+  const closeModal = () => setModalVisible(false);
+
+  useEffect(() => {
+    getPetInfo();
+  }, []);
 
   const getPetInfo = async () => {
     setLoadData(true);
@@ -46,20 +172,44 @@ const PetProfile = () => {
       setLoadData(false);
     }
   };
+  const savePhoto = async () => {
+    try {
+      const formData = new FormData();
+      formData.append("picture", {
+        uri: imageSource.uri,
+        type: "image/jpeg",
+        name: imageSource.fileName,
+      });
 
-  useEffect(() => {
-    getPetInfo();
-  }, []);
-  const rawGender = selectedPet?.pet_breed?.life_stages?.[0]?.gender;
-  const petGender =
-    rawGender === "male"
-      ? "Macho"
-      : rawGender === "female"
-      ? "Hembra"
-      : "Sin especificar";
+      const response = await apiFetcher.updatePicturePet(
+        selectedPet.id,
+        formData
+      );
+
+      if (response.code == 200) {
+        setSelectedPet({ ...selectedPet, picture: imageSource.uri });
+
+        Toast.show({
+          type: "success",
+          text1: "Foto actualizada",
+          text2: "La foto de tu mascota se ha actualizado correctamente.",
+        });
+      } else {
+        Toast.show({
+          type: "error",
+          text2: `Error`,
+          text1: `Error al guardar la foto`,
+        });
+      }
+    } catch (error) {
+      console.log("Ocurrió un error: ", error);
+    }
+  };
+
   const updatePet = async () => {
     setLoadData(true);
     try {
+      imageSource && savePhoto();
       const response = await apiFetcher.updatePet(selectedPet.id, petInfo);
 
       if (response.code === 200) {
@@ -90,173 +240,208 @@ const PetProfile = () => {
   }
 
   return (
-    <ScrollView style={{ flex: 1, backgroundColor: "#fff" }}>
-      <View
-        row
-        padding-20
-        style={{ borderBottomWidth: 1, borderBottomColor: Colors.secondGray }}
-      >
-        <View center>
-          <Avatar.Image source={{ uri: selectedPet?.picture }} size={100} />
-          <Text text80 color={Colors.gray} marginT-10>
-            Editar foto
-          </Text>
-        </View>
-
-        <View marginL-10 centerV>
-          <Text text40BL>{petInfo.name}</Text>
-          <Text text80L color={Colors.gray} marginT-5>
-            {`${selectedPet?.age} años | ${petGender} | ${selectedPet.pet_breed.description}`}
-          </Text>
-        </View>
-      </View>
-
-      <View width={"100%"} marginL-20>
-        <Text marginB-5 marginT-10 text80>
-          Nombre
-        </Text>
-        <TextInput
-          placeholder="Nombre"
-          placeholderTextColor="#000"
-          elevation={5}
-          value={petInfo.name}
-          onChangeText={(text) => setPetInfo({ ...petInfo, name: text })}
-          style={{
-            color: "#000",
-            height: 60,
-            width: "90%",
-            paddingLeft: 20,
-            justifyContent: "center",
-            backgroundColor: "#D6EFFF",
-            borderRadius: 4,
-          }}
-          autoCapitalize="none"
-        />
-
-        <Text marginB-5 marginT-20 text80>
-          Peso de tu mascota
-        </Text>
-        <TextInput
-          placeholder="Peso"
-          placeholderTextColor="#000"
-          elevation={5}
-          value={petInfo.weight}
-          onChangeText={(text) => setPetInfo({ ...petInfo, weight: text })}
-          style={{
-            color: "#000",
-            height: 60,
-            width: "90%",
-            paddingLeft: 20,
-            justifyContent: "center",
-            backgroundColor: "#D6EFFF",
-            borderRadius: 4,
-          }}
-          autoCapitalize="none"
-        />
+    <SafeAreaView style={{ backgroundColor: Colors.white, flex: 1 }}>
+      <ScrollView style={{ flex: 1, backgroundColor: Colors.white }}>
         <View
-          marginT-20
-          height={60}
-          width={'90%'}
-          paddingL-20
-          backgroundColor="#D6EFFF"
-          style={{
-            justifyContent: "center",
-            borderRadius: 4,
-          }}
-        >
-          <View row centerV>
-            <RadioButton.Android
-              value="yes"
-              status={petInfo.sterilized ? "checked" : "unchecked"}
-              color={Colors.primaryColor}
-              onPress={() =>
-                setPetInfo({ ...petInfo, sterilized: !petInfo.sterilized })
-              }
-            />
-            <Text marginL-10>¿Tu mascota está esterilizada?</Text>
-          </View>
-        </View>
-
-        <View
+          padding-10
           row
-          marginT-20
-          width={"90%"}
-          style={{ justifyContent: "space-between"}}
+          style={{
+            shadowColor: Colors.gray,
+            shadowOffset: { width: 0, height: 3 },
+            shadowOpacity: 0.3,
+            shadowRadius: 2,
+            elevation: 5,
+          }}
         >
-          <View row center>
-            <MaterialCommunityIcons
-              name="trash-can-outline"
-              size={24}
-              color={Colors.gray}
-            />
-            <Text color={Colors.gray} marginL-5>
-              Eliminar mascota
+          <View center>
+            {selectedPet?.picture ? (
+              <Image
+                height={120}
+                width={120}
+                borderRadius={60}
+                source={
+                  imageSource
+                    ? { uri: imageSource.uri }
+                    : { uri: selectedPet.picture }
+                }
+              />
+            ) : (
+              <WithoutPhoto />
+            )}
+            <TouchableOpacity
+              onPress={() => {
+                setModalVisible(true);
+              }}
+            >
+              <Text text80 color={Colors.gray} marginT-10>
+                Editar foto
+              </Text>
+            </TouchableOpacity>
+          </View>
+
+          <View marginL-10 centerV>
+            <Text text40BL>{selectedPet?.name}</Text>
+            <Text text80L color={Colors.gray} marginT-5>
+              {`${selectedPet?.age} años | ${selectedPet?.pet_breed?.life_stages?.[0]?.gender} | ${selectedPet?.pet_breed.name}`}
             </Text>
           </View>
-
-          <Button
-            backgroundColor={Colors.primaryColor}
-            label="Actualizar"
-            style={{ borderRadius: 10, padding: 15 }}
-            onPress={updatePet}
-          />
         </View>
-      </View>
 
-      <View paddingH-10>
-        <View marginB-10>
-          <Text text70BO marginT-5>
-            MIS DOCUMENTOS
+        <View width={"100%"} marginL-20>
+          <Text marginB-5 marginT-10 text80>
+            Nombre
           </Text>
-        </View>
-        <TouchableOpacity
-          onPress={() =>
-            navigation.navigate("IdInfoPet", { pet: selectedPet })
-          }
-        >
+          <TextInput
+            placeholder="Nombre"
+            placeholderTextColor="#000"
+            elevation={5}
+            value={petInfo.name}
+            onChangeText={(text) => setPetInfo({ ...petInfo, name: text })}
+            style={{
+              color: "#000",
+              height: 60,
+              width: "90%",
+              paddingLeft: 20,
+              justifyContent: "center",
+              backgroundColor: "#D6EFFF",
+              borderRadius: 4,
+            }}
+            autoCapitalize="none"
+          />
+
+          <Text marginB-5 marginT-20 text80>
+            Peso de tu mascota
+          </Text>
+          <TextInput
+            placeholder="Peso"
+            placeholderTextColor="#000"
+            elevation={5}
+            value={petInfo.weight}
+            onChangeText={(text) => setPetInfo({ ...petInfo, weight: text })}
+            style={{
+              color: "#000",
+              height: 60,
+              width: "90%",
+              paddingLeft: 20,
+              justifyContent: "center",
+              backgroundColor: "#D6EFFF",
+              borderRadius: 4,
+            }}
+            autoCapitalize="none"
+          />
           <View
-            row
-            spread
-            centerV
-            paddingV-15
-            width={"100%"}
-            style={{ borderBottomWidth: 0.2 }}
+            marginT-20
+            height={60}
+            width={"90%"}
+            paddingL-20
+            backgroundColor="#D6EFFF"
+            style={{
+              justifyContent: "center",
+              borderRadius: 4,
+            }}
           >
-            <View row gap-10 centerV marginL-10>
-              <Icon name="qrcode" size={25} color={Colors.black} />
-              <Text text70>ID digital de mi mascota</Text>
-            </View>
-            <View row gap-10 centerV>
-              <Icon name="chevron-right" size={25} color={Colors.black} />
-            </View>
-          </View>
-        </TouchableOpacity>
-        <TouchableOpacity
-          onPress={() => navigation.navigate("ProfileEditUser")}
-        >
-          <View
-            row
-            spread
-            centerV
-            paddingV-15
-            width={"100%"}
-            style={{ borderBottomWidth: 0.2 }}
-          >
-            <View row gap-10 centerV marginL-10>
-              <Icon
-                name="cards-playing-heart-outline"
-                size={25}
-                color={Colors.black}
+            <View row centerV>
+              <RadioButton.Android
+                value="yes"
+                status={petInfo.sterilized ? "checked" : "unchecked"}
+                color={Colors.primaryColor}
+                onPress={() =>
+                  setPetInfo({ ...petInfo, sterilized: !petInfo.sterilized })
+                }
               />
-              <Text text70>Cartilla de vacunación</Text>
-            </View>
-            <View row gap-10 centerV>
-              <Icon name="chevron-right" size={25} color={Colors.black} />
+              <Text marginL-10 text80BL>
+                ¿Tu mascota está esterilizada?
+              </Text>
             </View>
           </View>
-        </TouchableOpacity>
-      </View>
-    </ScrollView>
+
+          <View
+            row
+            marginT-20
+            width={"90%"}
+            style={{ justifyContent: "space-between" }}
+          >
+            <View row center>
+              <MaterialCommunityIcons
+                name="trash-can-outline"
+                size={24}
+                color={Colors.gray}
+              />
+              <Text color={Colors.gray} marginL-5>
+                Eliminar mascota
+              </Text>
+            </View>
+
+            <Button
+              backgroundColor={Colors.primaryColor}
+              label="Actualizar"
+              style={{ borderRadius: 10, padding: 15 }}
+              onPress={updatePet}
+            />
+          </View>
+        </View>
+
+        <View paddingH-10>
+          <View marginB-10>
+            <Text text70BO marginT-5>
+              MIS DOCUMENTOS
+            </Text>
+          </View>
+          <TouchableOpacity
+            onPress={() =>
+              navigation.navigate("IdInfoPet", { pet: selectedPet })
+            }
+          >
+            <View
+              row
+              spread
+              centerV
+              paddingV-15
+              width={"100%"}
+              style={{ borderBottomWidth: 0.2 }}
+            >
+              <View row gap-10 centerV marginL-10>
+                <Icon name="qrcode" size={25} color={Colors.black} />
+                <Text text70>ID digital de mi mascota</Text>
+              </View>
+              <View row gap-10 centerV>
+                <Icon name="chevron-right" size={25} color={Colors.black} />
+              </View>
+            </View>
+          </TouchableOpacity>
+          <TouchableOpacity
+            onPress={() => navigation.navigate("ProfileEditUser")}
+          >
+            <View
+              row
+              spread
+              centerV
+              paddingV-15
+              width={"100%"}
+              style={{ borderBottomWidth: 0.2 }}
+            >
+              <View row gap-10 centerV marginL-10>
+                <Icon
+                  name="cards-playing-heart-outline"
+                  size={25}
+                  color={Colors.black}
+                />
+                <Text text70>Cartilla de vacunación</Text>
+              </View>
+              <View row gap-10 centerV>
+                <Icon name="chevron-right" size={25} color={Colors.black} />
+              </View>
+            </View>
+          </TouchableOpacity>
+        </View>
+      </ScrollView>
+      <ImageOption
+        visible={modalVisible}
+        closeModal={closeModal}
+        selectImageFromLibrary={getPermissionsLibrary}
+        takePhoto={getPermissionsCamera}
+      />
+    </SafeAreaView>
   );
 };
 
